336d44f05c415f4d883ef8b720f8eb07e8e48f9a
Eliezer Ramirez <eliezer.ramirez@corporativo.local>
2024-10-24 16:18:40 -0400

    subiendo cambios a repo de github parte 2

--- src/Presentation/Controllers/CedulaController.ts
+++ src/Presentation/Controllers/CedulaController.ts
@@ -1,16 +1,13 @@
-import { Request, Response, Router } from 'express';
-import { ICedulaService } from '../../Application/Interfaces/Services/ICedulaService';
-import dotenv from 'dotenv';
 
-// Cargar el archivo apikey.env
-dotenv.config({ path: 'apikey.env' });
+import { Router, Request, Response } from 'express';
+import { ICedulaService } from '../../Application/Interfaces/Services/ICedulaService';
 
 export class CedulaController {
-    private readonly cedulaService: ICedulaService;
+    private cedulaService: ICedulaService;
     public router: Router;
 
-    // Obtener la API key desde las variables de entorno
-    private readonly validApiKey: string = process.env.API_KEY || '';
+    // Definir la API key válida
+    private readonly validApiKey: string = process.env.API_KEY || 'd93ca6497497cf7ea0f768a7e8c472235712b5312d633713e0f89101895f64d7';
 
     constructor(cedulaService: ICedulaService) {
         this.cedulaService = cedulaService;
@@ -24,8 +21,8 @@ export class CedulaController {
 
     // Función para verificar la API key
     private checkApiKey(req: Request): boolean {
-        const apiKeyFromHeader = req.headers['x-api-key'];
-        const apiKeyFromParams = req.query.apiKey;
+        const apiKeyFromHeader = req.headers['x-api-key'] as string;
+        const apiKeyFromParams = req.query.apiKey as string;
 
         // Verificar si la API key está en los headers o en los parámetros de la URL
         return apiKeyFromHeader === this.validApiKey || apiKeyFromParams === this.validApiKey;
@@ -38,18 +35,18 @@ export class CedulaController {
             return;
         }
 
-        const strCedula = req.params.strCedula;
-        
         try {
+            const strCedula = req.params.strCedula;
+            
+            // Llamar al servicio para buscar la cédula
             const cedula = await this.cedulaService.buscarCedulaOnline(strCedula);
-
+            
             if (!cedula) {
                 res.status(404).json({ message: 'Cedula incorrecta' });
                 return;
             }
 
             const formattedCedula = `${strCedula.slice(0, 3)}-${strCedula.slice(3, 10)}-${strCedula.slice(10)}`;
-
             res.json({
                 id: formattedCedula,
                 nombre: cedula.nombre,
@@ -64,9 +61,8 @@ export class CedulaController {
                 rutaFoto: cedula.rutaFoto
             });
         } catch (error) {
-            console.error('Error al buscar la cédula:', error instanceof Error ? error.message : error);
+            console.error('Error al buscar la cédula:', error);
             res.status(500).json({ message: 'Error interno del servidor' });
         }
     }
 }
-
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,26 @@
-import express from 'express';
-import CedulaRepository from './Infrastructure/Repositories/CedulaRepository';
-import { CedulaService } from './Application/Services/CedulaServices';
-import { CedulaController } from './Presentation/Controllers/CedulaController';
+import { CedulaService } from "./Application/Services/CedulaServices";
+import CedulaRepository from "./Infrastructure/Repositories/CedulaRepository";
+import { CedulaController } from "./Presentation/Controllers/CedulaController";
 
+const express = require('express');
+const dotenv = require('dotenv');
+
+
+// Cargar el archivo .env
+dotenv.config({ path: 'apikey.env' });
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Instancia del repositorio y el servicio
+// Instancia del repositorio
 const cedulaRepository = new CedulaRepository();
+
+// Instancia del servicio con el repositorio inyectado
 const cedulaService = new CedulaService(cedulaRepository);
 
-// Instancia del controlador
+// Instancia del controlador con el servicio inyectado
 const cedulaController = new CedulaController(cedulaService);
 
-app.use(express.json());
-
-// Usa las rutas del controlador
 app.use('/api/cedula', cedulaController.router);
 
 app.listen(port, () => {
