72ccc41684d8c70e0ff37700cf202ea63eb3ce27
Eliezer Ramirez <eliezer.ramirez@corporativo.local>
2024-10-25 11:19:13 -0400

    Arreglando problemas para despliegue

--- src/Presentation/Controllers/CedulaController.ts
+++ src/Presentation/Controllers/CedulaController.ts
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: './apikey.env' });
 
 import { Router, Request, Response } from 'express';
 import { ICedulaService } from '../../Application/Interfaces/Services/ICedulaService';
@@ -5,47 +6,39 @@ import { ICedulaService } from '../../Application/Interfaces/Services/ICedulaSer
 export class CedulaController {
     private cedulaService: ICedulaService;
     public router: Router;
-
-    // Definir la API key válida
-    private readonly validApiKey: string = process.env.API_KEY || 'd93ca6497497cf7ea0f768a7e8c472235712b5312d633713e0f89101895f64d7';
+    private readonly validApiKey: string;
 
     constructor(cedulaService: ICedulaService) {
         this.cedulaService = cedulaService;
         this.router = Router();
-        this.initializeRoutes();
+        this.validApiKey = process.env.API_KEY || 'd93ca6497497cf7ea0f768a7e8c472235712b5312d633713e0f89101895f64d7';
+        this.initializeRoutes(); // Llamada al método
     }
 
     private initializeRoutes() {
         this.router.get('/:strCedula', this.buscarCedula.bind(this));
     }
 
-    // Función para verificar la API key
+    // Método de verificación de la API key
     private checkApiKey(req: Request): boolean {
-        const apiKeyFromHeader = req.headers['x-api-key'] as string;
-        const apiKeyFromParams = req.query.apiKey as string;
-
-        // Verificar si la API key está en los headers o en los parámetros de la URL
+        const apiKeyFromHeader = req.headers['x-api-key'];
+        const apiKeyFromParams = req.query.apikey; // Cambiado a 'apikey'
+        
         return apiKeyFromHeader === this.validApiKey || apiKeyFromParams === this.validApiKey;
     }
 
     public async buscarCedula(req: Request, res: Response): Promise<void> {
-        // Verificar la API key antes de procesar la solicitud
         if (!this.checkApiKey(req)) {
             res.status(401).json({ message: 'API key inválida o faltante' });
             return;
         }
-
         try {
             const strCedula = req.params.strCedula;
-            
-            // Llamar al servicio para buscar la cédula
             const cedula = await this.cedulaService.buscarCedulaOnline(strCedula);
-            
             if (!cedula) {
                 res.status(404).json({ message: 'Cedula incorrecta' });
                 return;
             }
-
             const formattedCedula = `${strCedula.slice(0, 3)}-${strCedula.slice(3, 10)}-${strCedula.slice(10)}`;
             res.json({
                 id: formattedCedula,
